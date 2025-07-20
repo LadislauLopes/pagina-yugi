fc220a7d92e379faad4ca488f665a133da61c3b5
Ladislau Lopes <ladislau.contato.01@gmail.com>
2025-07-19 20:40:03 -0500

    projeto final

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
 const imagem_baner = document.getElementById("img_banner")
-
+const imagem_card_1 = document.getElementById("imagem_quem_somos")
+const imagem_card_2 = document.getElementById("o_que_e_yugi")
 const lista_imagens = [
   "exodia.svg",
+  "blue-eyes.svg",
   "fiendsmith.svg",
   "maliss.svg",
   "melodious.svg",
   "tenpai.svg",
-  "salamangreat.svg",
+  "crystron.svg",
+  "albaz.svg",
+  "centurion.svg",
 ]
 let positon_banner = 1
+const quantidade_na_lista = lista_imagens.length - 1
+
 const nextImageBanner = () => {
   imagem_baner.setAttribute(
     "src",
     `images/svg/decks/${lista_imagens[positon_banner]}`
   )
-  if (positon_banner === 5) {
+  console.log(quantidade_na_lista)
+  if (positon_banner === quantidade_na_lista) {
     positon_banner = 0
   } else {
     positon_banner += 1
@@ -23,12 +30,14 @@ const nextImageBanner = () => {
 
 let positon_cards = 1
 const nextImagecards = (caminho) => {
-  imagem_baner.setAttribute("src", `${caminho}img${[positon_cards]}`)
-  if (positon === 4) {
+  imagem_card_1.setAttribute("src", `images/png/turma/img${positon_cards}.jpg`)
+  imagem_card_2.setAttribute("src", `images/png/jogos/jogo${positon_cards}.jpg`)
+  if (positon_cards === 4) {
     positon_cards = 1
   } else {
     positon_cards += 1
   }
 }
+setInterval(nextImageBanner, 3000)
+// setInterval(nextImagecards, 6000)
 
-setInterval(nextImageBanner, 4000)
